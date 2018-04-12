497d146c8a501a6409790cab166e2e49cc84faf7
vibecastor <vibecastor@gmail.com>
2018-04-12 15:54:34 -0700

    added exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,25 @@
 'use strict'
 
+const exec = require('child_process').exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
+  let content = 'empty';
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
 
-function sleep(milliSeconds) {
-  let startTime = new Date().getTime();
-  while (new Date().getTime() < startTime + milliSeconds);
-}
+// function sleep(milliSeconds) {
+//   let startTime = new Date().getTime();
+//   while (new Date().getTime() < startTime + milliSeconds);
+// }
 
-sleep(10000);
-return 'Hello Start';
+// sleep(10000);
+// return 'Hello Start';
 
 function upload() {
   console.log("Request handle 'upload' was called.");
